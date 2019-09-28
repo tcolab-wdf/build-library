2a31314d1780a805f1b10b8d2d8e07863c38e65b
tcolab-wdf <45236355+tcolab-wdf@users.noreply.github.com>
2019-09-28 11:58:21 +0200

    Update getTrkorrFromgCTSHistory.groovy

--- getTrkorrFromgCTSHistory.groovy
+++ getTrkorrFromgCTSHistory.groovy
@@ -5,7 +5,7 @@ import groovy.json.JsonOutput
 def call(def server_name, def port, def repo_id, def auth_id) {
 	
     def trkorr = ""
-    def toolURL = 'http://'+server_name+':'+port+'/sap/bc/cts_abapvcs/repository/'+repo_id+'/getHistory'
+    def toolURL = 'http://'+server_name+XXX':'+port+'/sap/bc/cts_abapvcs/repository/'+repo_id+'/getHistory'
     String ABAPURL = toolURL.toURL()
     def resultJSON = 'results/ABAP_'+server_name+'_gCTS_history_'+env.BUILD_NUMBER+'.json'
     def resp = httpRequest authentication: auth_id, url: ABAPURL, outputFile: resultJSON
